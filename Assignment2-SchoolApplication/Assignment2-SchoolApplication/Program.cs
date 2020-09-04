525b280c8108b29b13d4de64ff1e589c074bc54a
David Ewens <david@ewens.se>
2020-09-04 13:56:51 +0200

    Part 5 Done

--- Assignment2-SchoolApplication/Assignment2-SchoolApplication/Program.cs
+++ Assignment2-SchoolApplication/Assignment2-SchoolApplication/Program.cs
@@ -27,7 +27,7 @@ namespace Assignment2_SchoolApplication
         
 
     }
-
+    /*
     public class Student // Part 1
     {
         string firstNameStudent;
@@ -58,34 +58,41 @@ namespace Assignment2_SchoolApplication
         }
 
     }
+    */
 
-    public class Teacher // Part 2
+    public class Student : Person
     {
-        string firstNameTeacher;
-        string lastNameTeacher;
-        DateTime dateOfBirthTeacher;
+        int studentId;
+        private static int studentIdCount = 1;
+
+        public Student(string firstName, string lastName, DateTime dateOfBirth) : base(firstName, lastName, dateOfBirth)
+        {
+            this.studentId = studentIdCount;
+            studentIdCount++;
+        }
+
+        public override string ToString()
+        {
+            return String.Concat(base.ToString(), " ", studentId);
+        }
+    }
+
+    public class Teacher : Person // Part 2
+    {
+
         int teacherId;
         private static int teacherIdCount = 1;
 
-        public Teacher(string firstName, string lastName, DateTime dateOfBirth)
-        {
-            this.firstNameTeacher = firstName;
-            this.lastNameTeacher = lastName;
-            this.dateOfBirthTeacher = dateOfBirth;
+        public Teacher(string firstName, string lastName, DateTime dateOfBirth) : base(firstName, lastName, dateOfBirth) {
             this.teacherId = teacherIdCount;
             teacherIdCount++;
         }
-        public override string ToString() // Return full name, DOB, Student ID in readable format
+        
+        // Return full name, DOB, ID in readable format
+        public override string ToString()
         {
-            string returnString = String.Concat(this.firstNameTeacher, " ", this.lastNameTeacher, " ", this.dateOfBirthTeacher.ToShortDateString(), " ", this.teacherId);
-            return returnString;
-        }
-        public int GetAge()
-        {
-            int ageInYears = 0;
-            TimeSpan daysSinceBirth = DateTime.Now - this.dateOfBirthTeacher;
-            ageInYears = daysSinceBirth.Days / 365;
-            return ageInYears;
+            //string returnString = String.Concat(firstNamePerson, " ", lastNamePerson, " ", dateOfBirthPerson.ToShortDateString(), " ", teacherId);
+            return String.Concat(base.ToString(), " ", teacherId);
         }
 
     }
@@ -149,5 +156,32 @@ namespace Assignment2_SchoolApplication
             }
             
         }
+
     }
+
+    // Part 5 - Person
+    public class Person
+    {
+        public string firstNamePerson; //{ get; set; }
+        public string lastNamePerson; //{ get; set; }
+        public DateTime dateOfBirthPerson; //{ get; set; }
+
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            firstNamePerson = firstName;
+            lastNamePerson = lastName;
+            dateOfBirthPerson = dateOfBirth;
+        }
+        public override string ToString()
+        {
+            return String.Concat(firstNamePerson, " ", lastNamePerson, " ", dateOfBirthPerson);
+        }
+        public int GetAge()
+        {
+            TimeSpan daysSinceBirth = DateTime.Now - this.dateOfBirthPerson;
+            int ageInYears = daysSinceBirth.Days / 365;
+            return ageInYears;
+        }
+    }
+
 }
